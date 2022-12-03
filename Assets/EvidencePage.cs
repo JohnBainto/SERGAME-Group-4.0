26e50ba14b02d5907c4d92a0116d0ef75680bb4d
ajmarcelo <andreajean1014@gmail.com>
2022-12-04 07:47:07 +0800

    Exploration Codex Implementation complete

--- Assets/EvidencePage.cs
+++ Assets/EvidencePage.cs
@@ -12,13 +12,50 @@ public class EvidencePage : MonoBehaviour
     [SerializeField]  GameObject sprite0;
 
     public void Setup(PageData pageData, string ch0_e, string ch1_e, string ch2_e,  int chNo) {
+        bool isAcquired = false;
+        
+        if (pageData.pageName.Contains("CH0 Evidence 1"))
+        {
+            if(ch0_e == "e1") isAcquired = true; 
+        }
+
+        if (pageData.pageName.Contains("CH1 Evidence 1"))
+        {
+            if(ch1_e.Contains("e1")) isAcquired = true; 
+        }
+        if (pageData.pageName.Contains("CH1 Evidence 2"))
+        {
+            if(ch1_e.Contains("e2")) isAcquired = true; 
+        }
+        if (pageData.pageName.Contains("CH1 Evidence 3"))
+        {
+            if(ch1_e.Contains("e3")) isAcquired = true;             
+        }
+
+        if (pageData.pageName.Contains("CH2 Evidence 1"))
+        {
+            if(ch2_e.Contains("e1")) isAcquired = true; 
+        }
+        if (pageData.pageName.Contains("CH2 Evidence 2"))
+        {
+            if(ch2_e.Contains("e2")) isAcquired = true; 
+        }
+        if (pageData.pageName.Contains("CH2 Evidence 3"))
+        {
+            if(ch2_e.Contains("e3")) isAcquired = true; 
+        }
         gameObject.SetActive(true);
         text0.text = pageData.pageContents[0];
         text1.text = pageData.pageContents[1];
         text2.text = pageData.pageContents[2];
         sprite0.GetComponent<Image>().sprite = pageData.pageSprites[0];
+        if(!isAcquired)
+        {
+            text0.text = pageData.pageContents[3];
+            text2.text = pageData.pageContents[4];
+            sprite0.GetComponent<Image>().sprite = pageData.pageSprites[0];            
+        }
     }
-
     public void Destroy() {
         gameObject.SetActive(false);
     }

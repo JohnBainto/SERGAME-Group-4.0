9450cfd94657b81c784c6631e1e252f59d4e427a
ajmarcelo <andreajean1014@gmail.com>
2022-12-03 23:51:38 +0800

    Update ChapterPage.cs
    - (temporarily removed) causes compilation error

--- Assets/ChapterPage.cs
+++ Assets/ChapterPage.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class ChapterPage : MonoBehaviour
 {
-    [SerializeField] public TextMeshProUGUI text0;
-    [SerializeField] public TextMeshProUGUI text1;
-    [SerializeField] public TextMeshProUGUI text2;
-    [SerializeField] public Sprite sprite0;
+    // [SerializeField] public TextMeshProUGUI text0;
+    // [SerializeField] public TextMeshProUGUI text1;
+    // [SerializeField] public TextMeshProUGUI text2;
+    // [SerializeField] public TextMeshProUGUI text2;
 
-    public void Setup(PageData pageData) {
-        gameObject.SetActive(true);
-        text0.text = pageData.pageContents[0];
-        text1.text = pageData.pageContents[1];
-        text2.text = pageData.pageContents[2];
-    }
+    // public void Setup(PageData pageData) {
+    //     gameObject.SetActive(true);
+    //     text0.text = pageData.pageContents[0];
+    //     text1.text = pageData.pageContents[1];
+    //     text2.text = pageData.pageContents[2];
+    // }
 }
